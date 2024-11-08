1538b676501810beac931ad282b817a1e2694a53
hsnwf15 <hsnlwfa15@gmail.com>
2024-11-08 21:54:03 +0800

    Menambahkan Kode untuk Menghitung Diskon

--- src/DiscountCalculator.java
+++ src/DiscountCalculator.java
@@ -8,7 +8,7 @@
  * @author Lenovo
  */
 public class DiscountCalculator extends javax.swing.JFrame {
-
+    
     /**
      * Creates new form DiscountCalculator
      */
@@ -36,11 +36,14 @@ public class DiscountCalculator extends javax.swing.JFrame {
         discountComboBox = new javax.swing.JComboBox<>();
         discountSlider = new javax.swing.JSlider();
         jLabel5 = new javax.swing.JLabel();
-        calculateButton = new javax.swing.JButton();
+        calculateDiscount = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         historyArea = new javax.swing.JTextArea();
-        jPanel3 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        finalPriceLabel = new javax.swing.JLabel();
+        savingsLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(242, 221, 38));
@@ -52,6 +55,7 @@ public class DiscountCalculator extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(250, 175, 45));
         jPanel1.setLayout(new java.awt.GridBagLayout());
 
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Masukkan Harga Asli:");
@@ -123,26 +127,30 @@ public class DiscountCalculator extends javax.swing.JFrame {
         gridBagConstraints.gridwidth = 2;
         jPanel1.add(jLabel5, gridBagConstraints);
 
-        calculateButton.setBackground(new java.awt.Color(249, 94, 5));
-        calculateButton.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        calculateButton.setForeground(new java.awt.Color(255, 255, 255));
-        calculateButton.setText("Hitung");
+        calculateDiscount.setBackground(new java.awt.Color(249, 94, 5));
+        calculateDiscount.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        calculateDiscount.setForeground(new java.awt.Color(255, 255, 255));
+        calculateDiscount.setText("Hitung");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 5;
         gridBagConstraints.gridwidth = 2;
         gridBagConstraints.insets = new java.awt.Insets(12, 0, 12, 0);
-        jPanel1.add(calculateButton, gridBagConstraints);
+        jPanel1.add(calculateDiscount, gridBagConstraints);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 5;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.VERTICAL;
         gridBagConstraints.ipadx = 35;
         gridBagConstraints.ipady = 21;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.PAGE_START;
+        gridBagConstraints.insets = new java.awt.Insets(10, 10, 10, 10);
         jPanel4.add(jPanel1, gridBagConstraints);
 
         jPanel2.setBackground(new java.awt.Color(122, 179, 24));
-        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Harga Akhir", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 1, 24), new java.awt.Color(255, 255, 255))); // NOI18N
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Riwayat Perhitungan Diskon", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 1, 18), new java.awt.Color(255, 255, 255))); // NOI18N
         jPanel2.setLayout(new java.awt.GridBagLayout());
 
         historyArea.setColumns(20);
@@ -158,30 +166,61 @@ public class DiscountCalculator extends javax.swing.JFrame {
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridy = 4;
         gridBagConstraints.ipadx = 40;
         gridBagConstraints.ipady = 40;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.PAGE_START;
+        gridBagConstraints.insets = new java.awt.Insets(0, 10, 15, 10);
         jPanel4.add(jPanel2, gridBagConstraints);
 
-        jPanel3.setLayout(new java.awt.GridBagLayout());
+        jLabel6.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Jumlah Penghematan:");
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        gridBagConstraints.insets = new java.awt.Insets(0, 10, 15, 10);
+        jPanel4.add(jLabel6, gridBagConstraints);
+
+        jLabel7.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel7.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("Harga Akhir:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        gridBagConstraints.insets = new java.awt.Insets(10, 10, 15, 10);
+        jPanel4.add(jLabel7, gridBagConstraints);
+
+        finalPriceLabel.setFont(new java.awt.Font("Roboto", 0, 24)); // NOI18N
+        finalPriceLabel.setForeground(new java.awt.Color(255, 255, 255));
+        finalPriceLabel.setText("-");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
-        gridBagConstraints.ipadx = 247;
-        gridBagConstraints.ipady = -21;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        jPanel4.add(jPanel3, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(0, 10, 15, 10);
+        jPanel4.add(finalPriceLabel, gridBagConstraints);
+
+        savingsLabel.setFont(new java.awt.Font("Roboto", 0, 24)); // NOI18N
+        savingsLabel.setForeground(new java.awt.Color(255, 255, 255));
+        savingsLabel.setText("-");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.insets = new java.awt.Insets(0, 10, 15, 10);
+        jPanel4.add(savingsLabel, gridBagConstraints);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, 754, Short.MAX_VALUE)
+            .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 772, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 425, Short.MAX_VALUE)
+            .addComponent(jPanel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 471, Short.MAX_VALUE)
         );
 
         pack();
@@ -223,20 +262,23 @@ public class DiscountCalculator extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton calculateButton;
+    private javax.swing.JButton calculateDiscount;
     private javax.swing.JTextField couponField;
     private javax.swing.JComboBox<String> discountComboBox;
     private javax.swing.JSlider discountSlider;
+    private javax.swing.JLabel finalPriceLabel;
     private javax.swing.JTextArea historyArea;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField priceField;
+    private javax.swing.JLabel savingsLabel;
     // End of variables declaration//GEN-END:variables
 }
